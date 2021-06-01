66e40716b4f92f6e8853c21386a7a20490cf38b8
Nicholas Huber <nickhuber327@gmail.com>
2021-05-31 19:17:44 -0600

    First commit

---
+++ .emacs.d/elpa/ox-ssh-20201217.317/ox-ssh-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from ox-ssh.el  -*- no-byte-compile: t -*-
+(define-package "ox-ssh" "20201217.317" "SSH Config Backend for Org Export Engine" '((emacs "24.4")) :commit "1b39849e3a315de95543eb3cf69c42fa33a8f5cd" :authors '(("Dante Catalfamo")) :maintainer '("Dante Catalfamo") :keywords '("outlines" "org" "ssh") :url "https://github.com/dantecatalfamo/ox-ssh")
